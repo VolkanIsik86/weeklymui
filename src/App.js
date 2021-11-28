04da021af39a5795320bf7b693cc8530fc061fb1
Volkan <s180103@student.dtu.dk>
2021-11-28 10:03:25 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import {Route, Switch} from "react-router-dom";
+import {Layout} from "./Layout";
+import Home from "./Home";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return(
+      <Layout>
+        <Switch>
+          <Route exact path={"/"} component={Home}/>
+          <Route render={() => <h1>404</h1>}/>
+        </Switch>
+      </Layout>
   );
+
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ThemeProvider} from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import {HashRouter} from "react-router-dom";
+import {createTheme} from "@material-ui/core";
+
+const theme = createTheme({
+    palette: {
+        type: "dark",
+    }
+});
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <HashRouter>
+                    <App />
+            </HashRouter>
+        </ThemeProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
