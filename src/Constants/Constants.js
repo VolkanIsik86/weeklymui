29afa5e98e4dc1aefb336f602e61e73f8520080a
Volkan Isik <volkanisik@live.dk>
2021-12-24 23:27:31 +0100

    Constants.js opdateret

--- src/Constants/Constants.js
+++ src/Constants/Constants.js
@@ -1,12 +1,12 @@
 //https://a-carreras-c.medium.com/development-and-production-variables-for-react-apps-c04af8b430a5
 const dev = {
     url: {
-        API_URL: 'http://localhost:47340'
+        API_URL: 'http://localhost:5000'
     }
 };
 const prod = {
     url: {
-        API_URL: 'http://localhost:5000'
+        API_URL: 'http://localhost:5001'
     }
 };
 export const config = process.env.NODE_ENV === 'development' ? dev : prod;
